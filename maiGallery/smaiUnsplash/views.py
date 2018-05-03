c2763d7dd47b5eb0d80625de0154c6328d2dd078
sami-mai <samirah.maison@gmail>
2018-05-03 16:01:52 +0300

    create view function for home page

--- maiGallery/smaiUnsplash/views.py
+++ maiGallery/smaiUnsplash/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# from .models import Category, Image, Location
+
 
 # Create your views here.
+def home(request):
+    title = "Sami-Mai Unsplash"
+    return render(request, 'index.html', {"title": title})
