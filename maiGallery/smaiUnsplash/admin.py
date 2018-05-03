13804d7e31ddcd3eba246d2b44d99ec2dafa61c7
sami-mai <samirah.maison@gmail>
2018-05-03 16:04:57 +0300

    register models

--- maiGallery/smaiUnsplash/admin.py
+++ maiGallery/smaiUnsplash/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Image, Location
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image)
