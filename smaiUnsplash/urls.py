f1db64fead048956db652d6f286ae604845ea970
sami-mai <samirah.maison@gmail>
2018-05-05 14:16:31 +0300

    update

--- smaiUnsplash/urls.py
+++ smaiUnsplash/urls.py
@@ -5,8 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$', views.home, name='home'),
-    # url(r'^photos/$', views.photos, name='photos')
+    url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^image/', views.image, name='image')
+    url(r'^image/(\d+)', views.image, name='image')
 ]
+
 
 # configure our urls.py to register the MEDIA_ROOT route.
 if settings.DEBUG:
